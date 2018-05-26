0dfd4115af4d281c8f562cca642d8cfa03dacd96
Nate B <nathanpbrust@gmail.com>
2018-05-26 16:03:58 -0500

    fixed repeating issue

--- index.js
+++ index.js
@@ -13,36 +13,35 @@ bot.on('message', (message) => {
     const args = message.content.slice().trim().split(/ +/g);
     const command = args.shift().toLowerCase();
     const argz = message.content.toLowerCase().split(/ +/g);
+    
+    if (message.author.id !== '448500187829370890') {
 
-    if (message.isMentioned(bot.users.get('448500187829370890'))) {
-        cmd.logmsg(`Hello, ${message.author.username}.`, message, bot);
-        message.channel.send(`Hello, ${message.author.username}.`);
-    }
+        if (message.isMentioned(bot.users.get('448500187829370890'))) {
+            cmd.logmsg(`Hello, ${message.author.username}.`, message, bot);
+            message.channel.send(`Hello, ${message.author.username}.`);
+        }
 
-    if (argz.indexOf('ravioli') > -1) {
-        cmd.logmsg('https://www.chefboyardee.com/sites/g/files/qyyrlu321/files/images/callouts/Chef_RavioliCallout_750x556.jpg', message, bot);
-        message.channel.send('https://www.chefboyardee.com/sites/g/files/qyyrlu321/files/images/callouts/Chef_RavioliCallout_750x556.jpg');
-    }
-    if (argz.indexOf('spaghetti') > -1) {
-        cmd.logmsg('https://www.chefboyardee.com/sites/g/files/qyyrlu321/files/images/callouts/Chef_SpaghettiCallout_750x556.jpg', message, bot);
-        message.channel.send('https://www.chefboyardee.com/sites/g/files/qyyrlu321/files/images/callouts/Chef_SpaghettiCallout_750x556.jpg');
-    } else {
-        if (argz.indexOf('meatballs') > -1) {
+        if (argz.indexOf('ravioli') > -1) {
+            cmd.logmsg('https://www.chefboyardee.com/sites/g/files/qyyrlu321/files/images/callouts/Chef_RavioliCallout_750x556.jpg', message, bot);
+            message.channel.send('https://www.chefboyardee.com/sites/g/files/qyyrlu321/files/images/callouts/Chef_RavioliCallout_750x556.jpg');
+        }
+        if (argz.indexOf('spaghetti') > -1) {
             cmd.logmsg('https://www.chefboyardee.com/sites/g/files/qyyrlu321/files/images/callouts/Chef_SpaghettiCallout_750x556.jpg', message, bot);
             message.channel.send('https://www.chefboyardee.com/sites/g/files/qyyrlu321/files/images/callouts/Chef_SpaghettiCallout_750x556.jpg');
+        } else {
+            if (argz.indexOf('meatballs') > -1) {
+                cmd.logmsg('https://www.chefboyardee.com/sites/g/files/qyyrlu321/files/images/callouts/Chef_SpaghettiCallout_750x556.jpg', message, bot);
+                message.channel.send('https://www.chefboyardee.com/sites/g/files/qyyrlu321/files/images/callouts/Chef_SpaghettiCallout_750x556.jpg');
+            }
         }
-    }
-    if (argz.indexOf('sauce') > -1) {
-        cmd.logmsg('https://www.seriouseats.com/recipes/images/2014/09/20140919-easy-italian-american-red-sauce-vicky-wasik-19-1500x1125.jpg', message, bot);
-        message.channel.send('https://www.seriouseats.com/recipes/images/2014/09/20140919-easy-italian-american-red-sauce-vicky-wasik-19-1500x1125.jpg');
-    }
+        if (argz.indexOf('sauce') > -1) {
+            cmd.logmsg('https://www.seriouseats.com/recipes/images/2014/09/20140919-easy-italian-american-red-sauce-vicky-wasik-19-1500x1125.jpg', message, bot);
+            message.channel.send('https://www.seriouseats.com/recipes/images/2014/09/20140919-easy-italian-american-red-sauce-vicky-wasik-19-1500x1125.jpg');
+        }
 
-    cmd.runHive(message, bot);
-
+        cmd.runHive(message, bot);
+    }
 });
 
-<<<<<<< HEAD
 bot.login(process.env.Token);
-=======
-bot.login(process.env.Token);
->>>>>>> 737e1e7829157c7faa0420f7d4222eea4cc01911
+
